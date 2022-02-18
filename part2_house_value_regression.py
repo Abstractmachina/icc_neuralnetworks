4d2435e09101ce97489844c182a8959e02ab6e94
njf21 <njf21@ic.ac.uk>
2022-02-18 17:20:32 +0000

    done up to constructor of 2.1

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -2,18 +2,19 @@ import torch
 import pickle
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 class Regressor():
 
-    def __init__(self, x, nb_epoch = 1000):
+    def __init__(self, x, number_of_epochs = 1000):
         # You can add any input parameters you need
         # Remember to set them with a default value for LabTS tests
-        """ 
+        """
         Initialise the model.
-          
+
         Arguments:
-            - x {pd.DataFrame} -- Raw input data of shape 
-                (batch_size, input_size), used to compute the size 
+            - x {pd.DataFrame} -- Raw input data of shape
+                (batch_size, input_size), used to compute the size
                 of the network.
             - nb_epoch {int} -- number of epoch to train the network.
 
@@ -25,9 +26,10 @@ class Regressor():
 
         # Replace this code with your own
         X, _ = self._preprocessor(x, training = True)
-        self.input_size = X.shape[1]
-        self.output_size = 1
-        self.nb_epoch = nb_epoch 
+        self.input_size = X.shape[1] # number of rows, but what does this do?
+        self.output_size = 1 # What does this do???
+        self.number_of_epochs = number_of_epochs
+        self.scaler = StandardScaler()
         return
 
         #######################################################################
@@ -35,14 +37,14 @@ class Regressor():
         #######################################################################
 
     def _preprocessor(self, x, y = None, training = False):
-        """ 
+        """
         Preprocess input of the network.
-          
+
         Arguments:
-            - x {pd.DataFrame} -- Raw input array of shape 
+            - x {pd.DataFrame} -- Raw input array of shape
                 (batch_size, input_size).
             - y {pd.DataFrame} -- Raw target array of shape (batch_size, 1).
-            - training {boolean} -- Boolean indicating if we are training or 
+            - training {boolean} -- Boolean indicating if we are training or
                 testing the model.
 
         Returns:
@@ -58,6 +60,33 @@ class Regressor():
         #######################################################################
 
         # Replace this code with your own
+
+        # add one hot encoding to allow the model to work with text categories
+        one_hot_area = pd.get_dummies(x['ocean_proximity'], prefix="area")
+        x = pd.merge(x, one_hot_area, left_index=True, right_index=True)
+
+        # We're going to fill the nan values with the average value of the column
+        x = x.fillna(x.mean())
+
+        # unnecessary column, we've one hot encoded it now
+        x = x.drop(['ocean_proximity'], axis=1)
+
+        # we want to preserve the column names if possible so just storing them here for now.
+        x_columns = list(x.columns)
+        one_hot_area_columns = list(one_hot_area.columns)
+        x_columns.extend(one_hot_area_columns)
+
+
+        # logic to separate out training data from test data
+        if training:
+            x = pd.DataFrame(self.scaler.fit_transform(x))
+        else:
+            x = pd.DataFrame(self.scaler.transform(x))
+
+        # merge the one hot encoded data with the standardised data
+        x = pd.merge(x, one_hot_area, left_index=True, right_index=True)
+        x.columns = x_columns     
+
         # Return preprocessed x and y, return None for y if it was None
         return x, (y if isinstance(y, pd.DataFrame) else None)
 
@@ -65,13 +94,13 @@ class Regressor():
         #                       ** END OF YOUR CODE **
         #######################################################################
 
-        
+
     def fit(self, x, y):
         """
         Regressor training function
 
         Arguments:
-            - x {pd.DataFrame} -- Raw input array of shape 
+            - x {pd.DataFrame} -- Raw input array of shape
                 (batch_size, input_size).
             - y {pd.DataFrame} -- Raw output array of shape (batch_size, 1).
 
@@ -91,13 +120,13 @@ class Regressor():
         #                       ** END OF YOUR CODE **
         #######################################################################
 
-            
+
     def predict(self, x):
         """
         Output the value corresponding to an input x.
 
         Arguments:
-            x {pd.DataFrame} -- Raw input array of shape 
+            x {pd.DataFrame} -- Raw input array of shape
                 (batch_size, input_size).
 
         Returns:
@@ -121,7 +150,7 @@ class Regressor():
         Function to evaluate the model accuracy on a validation dataset.
 
         Arguments:
-            - x {pd.DataFrame} -- Raw input array of shape 
+            - x {pd.DataFrame} -- Raw input array of shape
                 (batch_size, input_size).
             - y {pd.DataFrame} -- Raw ouput array of shape (batch_size, 1).
 
@@ -142,8 +171,8 @@ class Regressor():
         #######################################################################
 
 
-def save_regressor(trained_model): 
-    """ 
+def save_regressor(trained_model):
+    """
     Utility function to save the trained regressor model in part2_model.pickle.
     """
     # If you alter this, make sure it works in tandem with load_regressor
@@ -152,8 +181,8 @@ def save_regressor(trained_model):
     print("\nSaved model in part2_model.pickle\n")
 
 
-def load_regressor(): 
-    """ 
+def load_regressor():
+    """
     Utility function to load the trained regressor model in part2_model.pickle.
     """
     # If you alter this, make sure it works in tandem with save_regressor
@@ -164,17 +193,17 @@ def load_regressor():
 
 
 
-def RegressorHyperParameterSearch(): 
+def RegressorHyperParameterSearch():
     # Ensure to add whatever inputs you deem necessary to this function
     """
-    Performs a hyper-parameter for fine-tuning the regressor implemented 
+    Performs a hyper-parameter for fine-tuning the regressor implemented
     in the Regressor class.
 
     Arguments:
         Add whatever inputs you need.
-        
+
     Returns:
-        The function should return your optimised hyper-parameters. 
+        The function should return your optimised hyper-parameters.
 
     """
 
@@ -197,15 +226,15 @@ def example_main():
     # Use pandas to read CSV data as it contains various object types
     # Feel free to use another CSV reader tool
     # But remember that LabTS tests take Pandas Dataframe as inputs
-    data = pd.read_csv("housing.csv") 
+    data = pd.read_csv("housing.csv")
 
     # Spliting input and output
     x_train = data.loc[:, data.columns != output_label]
     y_train = data.loc[:, [output_label]]
 
     # Training
-    # This example trains on the whole available dataset. 
-    # You probably want to separate some held-out data 
+    # This example trains on the whole available dataset.
+    # You probably want to separate some held-out data
     # to make sure the model isn't overfitting
     regressor = Regressor(x_train, nb_epoch = 10)
     regressor.fit(x_train, y_train)
@@ -218,4 +247,3 @@ def example_main():
 
 if __name__ == "__main__":
     example_main()
-
